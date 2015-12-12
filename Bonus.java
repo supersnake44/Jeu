28dfece0b39d9295aeb9f2d607d871c851bbf9b0
supersnake44 <joehjj96@gmail.com>
2015-12-12 23:45:13 +0100

    Create Bonus.java

---
+++ Bonus.java
@@ -0,0 +1,22 @@
+import java.awt.*; 
+import javax.swing.*; 
+import java.awt.event.*; 
+import java.io.*; 
+import java.awt.geom.*; 
+import java.awt.image.*;
+
+public class Bonus extends{
+    
+    public Bonus(){
+		
+		
+		
+		
+	}
+	
+	public int effetAleat(){
+		return (int)Math.random();
+	}
+}
+
+
